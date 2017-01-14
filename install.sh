09dd0a4dc4a029c26b2b657487d0ceb583dfb08a
SegFault42 <you@example.com>
2017-01-14 12:54:10 +0100

    first commit

---
+++ install.sh
@@ -0,0 +1,17 @@
+sudo apt-get update
+#=================================Developement=================================
+sudo apt-get install build-essential
+sudo apt-get install terminator
+sudo apt-get install zsh
+sudo apt-get install vim
+sudo apt-get install clang
+sudo apt-get install git
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+curl http://install.sublivim.com | sh
+#=====================================Tools====================================
+sudo apt-get install qbittorent
+sudo apt-get install filezilla
+sudo apt-get install gparted
+sudo apt-get install htop
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
